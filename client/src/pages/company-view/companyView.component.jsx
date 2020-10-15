6e1b5c55958d145d8b4dbc1e9e99606866751d1a
Jacob Bathan <jacobathan@gmail.com>
2020-10-14 23:52:58 -0700

    test

--- client/src/pages/company-view/companyView.component.jsx
+++ client/src/pages/company-view/companyView.component.jsx
@@ -5,7 +5,7 @@ class CompanyView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      ticker: 'TSLA',
+      ticker: 'SBUX',
       company: {},
     };
   }
