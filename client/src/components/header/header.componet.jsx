491ed7074c8a565a4d0d64e0b8c605db457ca47b
Jacob Bathan <jacobbathan@gmail.com>
2020-10-19 00:00:06 -0700

    add

--- client/src/components/header/header.componet.jsx
+++ client/src/components/header/header.componet.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Header = () => <div>Hello user! Welcome Back.</div>;
+const Header = () => (
+  <div>
+    Hello user! Welcome Back.
+    <Link to="/">
+      <div>
+        <ul>Home</ul>
+      </div>
+    </Link>
+    <Link to="/company">
+      <div>
+        <ul>Company View</ul>
+      </div>
+    </Link>
+  </div>
+);
 
 export default Header;
