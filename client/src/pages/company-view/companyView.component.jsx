edee041caf5b02f5ff69898c4939e8837ad48add
Jacob Bathan <jacobbathan@gmail.com>
2020-10-31 13:26:22 -0700

    search company working

--- client/src/pages/company-view/companyView.component.jsx
+++ client/src/pages/company-view/companyView.component.jsx
@@ -5,20 +5,20 @@ class CompanyView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      ticker: 'TSLA',
+      ticker: '',
       company: {},
     };
   }
 
-  handleChange = evt => {
+  handleChange = (evt) => {
     this.setState({
-      [evt.target.name]: evt.target.value
-    })
-  }
+      [evt.target.name]: evt.target.value,
+    });
+  };
 
   searchCompany = () => {
-    this.getCompanyData(this.state.ticker)
-  }
+    this.getCompanyData(this.state.ticker);
+  };
 
   componentDidMount = () => {
     this.getCompanyData(this.state.ticker);
@@ -42,16 +42,39 @@ class CompanyView extends React.Component {
     return (
       <div>
         <div>
-          <input type="text" name="ticker" value={this.state.ticker} onChange={this.handleChange} />
-          <button type="button" onClick={this.searchCompany}>Search</button>
+          <input
+            type="text"
+            name="ticker"
+            value={this.state.ticker}
+            onChange={this.handleChange}
+          />
+          <button type="button" onClick={this.searchCompany}>
+            Search
+          </button>
         </div>
-    <div>Data sheet for {company.ticker}</div>
-        <div>
-          Address: {company.address}, {company.city}
-        </div>
-        <div>Description: {company.description}</div>
-        <div>Industry: {company.finnhubIndustry}</div>
-        <div>Total Employees: {company.employeeTotal}</div>
+        {Object.keys(company).length === 0 ? (
+          <div>
+            Please search for a valid company without the ticker symbol ($).
+          </div>
+        ) : (
+          <div>
+            <div>Data sheet for ${company.ticker}</div>
+            <div>
+              {!company.logo ? (
+                'Image not found'
+              ) : (
+                <img src={company.logo} alt="Company logo" />
+              )}
+            </div>
+            <div>
+              Address: {company.address}, {company.city}, {company.state}{' '}
+              {company.country}
+            </div>
+            <div>Description: {company.description}</div>
+            <div>Industry: {company.finnhubIndustry}</div>
+            <div>Total Employees: {company.employeeTotal}</div>
+          </div>
+        )}
       </div>
     );
   }
