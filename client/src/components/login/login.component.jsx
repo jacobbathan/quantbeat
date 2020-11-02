0d0c58212d32d6989902dcd1f527c41b1ee6d7b2
Jacob Bathan <jacobbathan@gmail.com>
2020-11-01 21:01:54 -0800

    added loginsignup components

---
+++ client/src/components/login/login.component.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import FormInput from '../../components/form-input/form-input.component';
+import CustomButton from '../../components/custom-button/custom-button.component';
+import './login.styles.scss';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { email, password } = this.state;
+    try {
+      //   signup endpoint
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
+    this.setState({
+      email: '',
+      password: '',
+    });
+  };
+
+  handleChange = (evt) => {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="login">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            label="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            name="password"
+            type="password"
+            label="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            required
+          />
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign In With Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,63 +1,82 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, makeStyles } from '@material-ui/core';
 import phone from '../../assets/phone.jpeg';
 import research from '../../assets/research.jpeg';
 import stocks from '../../assets/stocks.jpg';
 import './homepage.styles.scss';
 
-const Homepage = () => (
-  <div>
-    <Typography variant="h2" className="header">
-      Sign up today to get your pulse on the market.
-    </Typography>
-    <Grid container>
-      <Grid item xs={6}>
-        <Typography variant="h6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis cursus
-          purus quam. Nulla nec porta velit, et porta est. Pellentesque non diam
-          quis neque egestas auctor. Pellentesque volutpat arcu in diam iaculis
-          ultricies. Pellentesque egestas, ligula eu placerat lacinia, odio
-          augue suscipit enim, id convallis mi urna nec mi. Aliquam justo erat,
-          ullamcorper ac ligula eget, accumsan sollicitudin magna. Class aptent
-          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-          himenaeos.
-        </Typography>
+const useStyles = makeStyles({
+  header: {
+    textAlign: 'center',
+    marginBottom: '5vh',
+  },
+  bodyText: {
+    margin: '15vh auto',
+  },
+  image: {
+    height: '400px',
+    width: 'auto',
+    marginTop: '5vh',
+  },
+});
+
+const Homepage = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Typography variant="h2" className={classes.header}>
+        Sign up today to get your pulse on the market.
+      </Typography>
+      <Grid container>
+        <Grid item xs={6}>
+          <Typography variant="h6" className={classes.bodyText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis cursus
+            purus quam. Nulla nec porta velit, et porta est. Pellentesque non
+            diam quis neque egestas auctor. Pellentesque volutpat arcu in diam
+            iaculis ultricies. Pellentesque egestas, ligula eu placerat lacinia,
+            odio augue suscipit enim, id convallis mi urna nec mi. Aliquam justo
+            erat, ullamcorper ac ligula eget, accumsan sollicitudin magna. Class
+            aptent taciti sociosqu ad litora torquent per conubia nostra, per
+            inceptos himenaeos.
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <img src={phone} className={classes.image} alt="phone" />
+        </Grid>
+        <Grid item xs={6}>
+          <img src={research} className={classes.image} alt="research" />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h6" className={classes.bodyText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis cursus
+            purus quam. Nulla nec porta velit, et porta est. Pellentesque non
+            diam quis neque egestas auctor. Pellentesque volutpat arcu in diam
+            iaculis ultricies. Pellentesque egestas, ligula eu placerat lacinia,
+            odio augue suscipit enim, id convallis mi urna nec mi. Aliquam justo
+            erat, ullamcorper ac ligula eget, accumsan sollicitudin magna. Class
+            aptent taciti sociosqu ad litora torquent per conubia nostra, per
+            inceptos himenaeos.
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h6" className={classes.bodyText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis cursus
+            purus quam. Nulla nec porta velit, et porta est. Pellentesque non
+            diam quis neque egestas auctor. Pellentesque volutpat arcu in diam
+            iaculis ultricies. Pellentesque egestas, ligula eu placerat lacinia,
+            odio augue suscipit enim, id convallis mi urna nec mi. Aliquam justo
+            erat, ullamcorper ac ligula eget, accumsan sollicitudin magna. Class
+            aptent taciti sociosqu ad litora torquent per conubia nostra, per
+            inceptos himenaeos.
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <img src={stocks} className={classes.image} alt="stocks" />
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <img src={phone} className="image" alt="phone" />
-      </Grid>
-      <Grid item xs={6}>
-        <img src={research} className="image" alt="research" />
-      </Grid>
-      <Grid item xs={6}>
-        <Typography variant="h6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis cursus
-          purus quam. Nulla nec porta velit, et porta est. Pellentesque non diam
-          quis neque egestas auctor. Pellentesque volutpat arcu in diam iaculis
-          ultricies. Pellentesque egestas, ligula eu placerat lacinia, odio
-          augue suscipit enim, id convallis mi urna nec mi. Aliquam justo erat,
-          ullamcorper ac ligula eget, accumsan sollicitudin magna. Class aptent
-          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-          himenaeos.
-        </Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography variant="h6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis cursus
-          purus quam. Nulla nec porta velit, et porta est. Pellentesque non diam
-          quis neque egestas auctor. Pellentesque volutpat arcu in diam iaculis
-          ultricies. Pellentesque egestas, ligula eu placerat lacinia, odio
-          augue suscipit enim, id convallis mi urna nec mi. Aliquam justo erat,
-          ullamcorper ac ligula eget, accumsan sollicitudin magna. Class aptent
-          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-          himenaeos.
-        </Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <img src={stocks} className="image" alt="stocks" />
-      </Grid>
-    </Grid>
-  </div>
-);
+    </div>
+  );
+};
 
 export default Homepage;
